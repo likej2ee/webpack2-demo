bf2bba8e1b01cb19ce4043b8e774a89a3504a28f
谢佳倩 <likej2ee@qq.com>
2017-03-15 16:09:05 +0800

    更新配置

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,9 +5,6 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     plugins: [
         new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            },
             __DEV__: true,
             __PRODUCTION__: false
         }),
