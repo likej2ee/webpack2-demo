f6665d1cf39e8e9c9d25476e1f4b6f0c335814f2
谢佳倩 <likej2ee@qq.com>
2017-04-14 23:39:15 +0800

    聊天弹幕

--- pages/views/demo/demo.js
+++ pages/views/demo/demo.js
@@ -1,11 +1,53 @@
 import './demo.scss';
 // import _ from 'lodash';
-// import Velocity from 'velocity';
+import Velocity from 'velocity';
 // import axios from 'axios';
+import $ from 'jquery';
 
 new Vue({
     el: '#example',
     data: {
-        name: 'hello world!'
+        text: '阿嘎神龙斗士来得及发熟练度解放路撒地方水电费阿斯蒂芬撒地方撒地方',
+        list: [1, 212312312, 3, '阿斯顿发斯蒂芬哈萨克东方红卡萨丁说的'],
+        isScroll: false
+    },
+    methods: {
+        sendMessage: function() {
+            this.list.push(this.text);
+
+            setTimeout(function() {
+                var $list = $('#j-message-list');
+                var $items = $list.find('.j-message')
+                var $currentItem = $items.last();
+
+                $currentItem.parent().fadeIn(1000);
+
+                var itemHeight = $currentItem.outerHeight();
+                var itemMarginBottom = parseInt($currentItem.css('margin-bottom'));
+
+
+                // $items.each(function (i) {
+                //     console.log($(this).offset().top);
+                //     var $this = $(this)
+                //     var offsetTop = $this.offset().top
+                //     if (offsetTop <= 0) {
+                //         $('#j-message-shadow').width($this.outerWidth());
+                //     }
+                // })
+
+                $list.stop(true, true).animate({
+                    scrollTop: $list.scrollTop() + itemHeight + itemMarginBottom
+                }, {
+                    speed: 5000,
+                    easing: 'linear'
+                })
+
+            })
+            // Velocity($list[0], {
+            //     translateY: -itemHeight
+            // }, {
+            //     duration: 500
+            // });
+        }
     }
 })
