df608ebd00f1187638e85ae30771258e5d99b364
谢佳倩 <likej2ee@qq.com>
2017-04-17 16:47:41 +0800

    更换页面源目录，使用page

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
     constants: {
-        sourceCodeRoot: 'pages', // 开发源文件目录
+        sourceCodeRoot: 'page', // 开发源文件目录
         assetsRoot: 'assets', // 资源根目录
         webpackManifest: 'webpack-manifest.json', // 配合gulp任务管理资源cache的映射地址
         libManifest: 'lib-manifest.json' // 独立打包的库文件的映射地址
