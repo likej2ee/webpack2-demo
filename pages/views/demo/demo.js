2a9eff08828fdd29a8a2abcdffc39932c8c6710f
谢佳倩 <likej2ee@qq.com>
2017-11-13 15:33:47 +0800

    重置demo页面

--- pages/views/demo/demo.js
+++ pages/views/demo/demo.js
@@ -1,30 +1,20 @@
 import './demo.scss'
-import specsData from './data-specs.js'
-import ProductSpec from './product-spec.js'
 
-let myProductSpec = null;
 let vm = new Vue({
     el: '#app',
     data: {
-        specs: null, // 约定的规格数据
-        specValueStatus: {}, // 规格值对应的渲染状态集合
-        selectedCommodityId: 0, // 选择完成后对应的唯一货品id
-        selectedSpecValueArr: null, // 已选择的规格值文本数组
-        unselectedSpecKeyArr: null, // 未选择的规格键名称文本数组
+        message: '2s后内容变化'
     },
     computed: {},
     methods: {
         init() {
-            myProductSpec = new ProductSpec()
-            myProductSpec.init(specsData)
-            this.specValueStatus = myProductSpec.specValueStatus
-            this.specs = specsData
+            this.changeMessage()
         },
-
-        // 选择规格选项
-        chooseSpecOption(specKey, specOption) {
-            myProductSpec.chooseSpecOption(specKey, specOption, this)
-        },
+        changeMessage() {
+            setTimeout(() => {
+                this.message = 'change message'
+            }, 2000)
+        }
     }
 })
 
